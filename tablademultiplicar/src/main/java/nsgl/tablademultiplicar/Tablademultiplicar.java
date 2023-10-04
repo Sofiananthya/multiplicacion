a6bcea80ebdce700a32d5797b845d2bd1ff9efc0
Sofiananthya <145470302+Sofiananthya@users.noreply.github.com>
2023-10-04 07:16:34 -0600

    Add files via upload

---
+++ tablademultiplicar/src/main/java/nsgl/tablademultiplicar/Tablademultiplicar.java
@@ -0,0 +1,15 @@
+private void Tablademultiplicar(java.awt.event.MouseEvent evt) 
+{
+
+package nsgl.tablademultiplicar;
+
+/**
+ *
+ * @author User
+ */
+public class Tablademultiplicar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
